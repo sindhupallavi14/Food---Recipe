6ddb77774c87626f96ca4e3ffafb5de2fec9de63
sindhupallavi14 <sindhuramanadula14@gmail.com>
2024-12-22 18:47:56 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { Has, HashRouterhRouter } from 'react-router-dom';
 import { FoodRecipeGlobalState } from './Components/Context/context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <FoodRecipeGlobalState>
       <App />
     </FoodRecipeGlobalState>  
-  </BrowserRouter>
+  </HashRouter>
  
 );
 
